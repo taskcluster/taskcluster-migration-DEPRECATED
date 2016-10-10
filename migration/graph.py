f3c715f82c85ad5f6966e85a4d31a109b565cbb8
Dustin J. Mitchell <dustin@mozilla.com>
2016-10-10 21:04:44 +0000

    show title on hover

--- migration/graph.py
+++ migration/graph.py
@@ -4,8 +4,8 @@ def make_graph(workgraph):
     dot = Digraph(comment="Work Graph")
     dot.body.append('rankdir=LR')
 
-    for name in workgraph:
-        dot.node(name, name)
+    for name, workitem in workgraph.iteritems():
+        dot.node(name, name, {'tooltip': workitem.title})
 
     for name, workitem in workgraph.iteritems():
         dot.edges((name, dep) for dep in workitem.dependencies)
