642a2d742890d455bfde0d4809cbf38f4216c17d
Dustin J. Mitchell <dustin@mozilla.com>
2016-10-20 00:26:17 +0000

    support S3 uploads

--- migration-preset/index.js
+++ migration-preset/index.js
@@ -10,4 +10,49 @@ preset.module.loaders.push({
   include: path.resolve(__dirname, '..', 'workgraph'),
   loaders: ['json', path.resolve(__dirname, 'loader.js')],
 });
+
+/* set up S3 publishing, if it's desired */
+if (process.env.PUBLISH_BUCKET) {
+  const request = require('sync-request');
+  const S3Plugin = require('webpack-s3-plugin');
+
+  if (!process.env.TASK_ID) {
+    throw new Error("publishing must take place in a task");
+  }
+
+  const region = process.env.PUBLISH_REGION;
+  if (!region) {
+    throw new Error("set PUBLISH_REGION");
+  }
+
+  const bucket = process.env.PUBLISH_BUCKET;
+  if (!bucket) {
+    throw new Error("set PUBLISH_BUCKET");
+  }
+
+  // get credentials
+  const response = request(
+      'GET',
+      'http://taskcluster/auth/v1/aws/s3/read-write/' + bucket + '/');
+  if (response.statusCode != 200) {
+    throw new Error(response.getBody());
+  }
+  const creds = JSON.parse(response.getBody()).credentials;
+  console.error("AWS credentials for " + bucket + " downloaded; accessKeyId is " + creds.accessKeyId);
+
+  preset.plugins.push(new S3Plugin({
+    s3Options: {
+      accessKeyId: creds.accessKeyId,
+      secretAccessKey: creds.secretAccessKey,
+      sessionToken: creds.sessionToken,
+      region: region,
+    },
+    s3UploadOptions: {
+      Bucket: bucket,
+      // we can't call PutObjectACL with STS credentials, so we can't use public-read
+      ACL: 'private',
+    }
+  }));
+}
+
 module.exports = preset;
